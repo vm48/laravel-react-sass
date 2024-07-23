dbd0758e3684db08bbac91083bd889a751049357
vm48 <v.m48@yandex.ru>
2024-07-23 14:36:21 +0300

    Create FeatureResource

--- app/Http/Controllers/Feature1Controller.php
+++ app/Http/Controllers/Feature1Controller.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\FeatureResource;
 use App\Models\Feature;
+use App\Models\UsedFeature;
 use Illuminate\Http\Request;
 
 class Feature1Controller extends Controller
@@ -27,5 +29,31 @@ class Feature1Controller extends Controller
 
   public function calculate(Request $request)
   {
+    $user = $request->user();
+    if ($user->available_credits < $this->feature->required_credits) {
+      return back();
+    }
+    $data    = $request->validate([
+      'number1' => [
+        'required',
+        'numeric',
+      ],
+      'number2' => [
+        'required',
+        'numeric',
+      ],
+    ]);
+    $number1 = (float) $data['number1'];
+    $number2 = (float) $data['number2'];
+    $user->decreaseCredits($this->feature->required_credits);
+
+    UsedFeature::query()->create([
+      'feature_id' => $this->feature->id,
+      'user_id'    => $user->id,
+      'credits'    => $this->feature->required_credits,
+      'data'       => $data,
+    ]);
+
+    return to_route('feature1.index')->with('answer', $number1 + $number2);
   }
 }
